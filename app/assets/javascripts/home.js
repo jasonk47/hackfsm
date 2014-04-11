dd6152b24f5f16d0bcb5a37388e3a50538ae024d
Jason Khoe <jkhoe@kabam.com>
2014-04-11 14:38:37 -0700

    More timeline junk

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
   $("#timeline-select").on("activate.bs.scrollspy", function() {
-    $(".collapse:not('.u-invoked'):not('.active')").collapse('hide');
     $("#timeline-select > ul > .active > ul").collapse('show');
   });
 
   $("#timeline-select > ul > li > a").on("click", function() {
     $(this).siblings("ul").addClass("u-invoked")
+  });
+
+  $(".node .text a").on("click", function() {
+    $(this).parent().siblings(".infobox").toggle(0);
+    return false;
   });
 });
