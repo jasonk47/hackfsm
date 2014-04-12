18e027540bc2c711fe5936320c95e03ccf77f60c
vagrant <vagrant@precise32.(none)>
2014-04-12 03:35:04 +0000

    Method that gets image url or xml content

--- app/helpers/timeline_helper.rb
+++ app/helpers/timeline_helper.rb
@@ -6,7 +6,7 @@ module TimelineHelper
     app_info = File.new('app_info.dat', 'r')
     app_id = app_info.gets.strip
     app_key = app_info.gets.strip
-    puts "app_id: #{app_id} app_key: #{app_key}"
+    # puts "app_id: #{app_id} app_key: #{app_key}"
     uri = URI("https://apis.berkeley.edu/solr/fsm/select?q=#{request}&wt=json&app_id=#{app_id}&app_key=#{app_key}")
     resp = Net::HTTP.start(uri.host, uri.port,
       :use_ssl => uri.scheme == 'https') do |http|
@@ -16,4 +16,35 @@ module TimelineHelper
     end
     return JSON.parse(resp.body)
   end
+  
+  # Returns the full hash of data with id
+  def get_hash_from_id(id)
+    id = '%22' << id << '%22'
+    begin
+      return get_from_fsm_api("id:#{id}")['response']['docs'][0]
+    rescue
+      print "#{id} is not a valid ID"
+    end
+  end
+  
+  # If id refers to image, returns list of image URLs
+  # If id refers to document, returns full text from TEI URL
+  def get_data_from_id(id)
+    data_hash = get_hash_from_id(id)
+    
+    if data_hash == nil
+      print "#{id} is not a valid ID\n"
+      return
+    end
+    
+    image_urls = data_hash['fsmImageUrl']
+    tei_url = data_hash['fsmTeiUrl'][0]
+    if image_urls != nil
+      return image_urls
+    elsif tei_url != nil
+      return Net::HTTP.get_response(URI.parse(tei_url)).body
+    end
+  end
+  
+  
 end
