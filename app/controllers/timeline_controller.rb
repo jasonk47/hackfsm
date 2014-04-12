12d0f52306b710a31bcc73fe7a7160647abf9888
Nicholas Chang <nnchang01@gmail.com>
2014-04-12 03:15:04 -0700

    [timeline_controller.rb] pretty documents
    added a bunch of nokogiri-based code in order to make XML
    documents more readable.  If I do say so myself, it works
    wonderfully.

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -1,4 +1,5 @@
 require 'net/http'
+require 'nokogiri'
 
 class TimelineController < ApplicationController
   include TimelineHelper
@@ -7,11 +8,11 @@ class TimelineController < ApplicationController
     id = params[:id]
 
     @fields_to_check = {
-      "Creator"       => "fsmCreator",
-      "Date"          => "fsmDateCreated",
-      "Resource Type" => "fsmTypeOfResource",
-      "Note"          => "fsmNote",
-      "Related Title" => "fsmRelatedTitle",
+      "Creator"             => "fsmCreator",
+      "Date"                => "fsmDateCreated",
+      "Resource Type"       => "fsmTypeOfResource",
+      "Note"                => "fsmNote",
+      "Related Title"       => "fsmRelatedTitle",
       "Archive Identifier"  => "fsmIdentifier",
       "Related Identifier"  => "fsmRelatedIdentifier",
       "Archive Location"    => "fsmPhysicalLocation"
@@ -19,6 +20,35 @@ class TimelineController < ApplicationController
 
     @info = get_hash_from_id(id)
     @content = get_data_from_id(id)
+    if @content.kind_of? String # this is xml
+      parsed_doc = Nokogiri::XML(@content)
+      root_children = parsed_doc.root.children
+      text = nil
+      for child in root_children
+        if child.element? and child.name.downcase == 'text'
+          text = child
+        end
+      end
+      for child in text.children.children
+        if child.element? and child.name.downcase == 'div1'
+          child.name = 'div'
+          for subchild in child.children
+            puts subchild.name
+            if subchild.element? and subchild.name.downcase == 'head'
+              subchild.name = 'h3'
+            end
+            if subchild.element? and subchild.name.downcase == 'p'
+              for subsubchild in subchild.children
+                if subsubchild.element? and subsubchild.name.downcase == 'emph'
+                  subchild.name = 'strong'
+                end
+              end
+            end
+          end
+        end
+      end
+      @content = text.to_s.html_safe
+    end
   end
 
 end
